dcd3fb90e00c4a6a029151f70486b5f0881bd391
limhawjia <hawjiaa@gmail.com>
2020-01-21 22:27:23 +0800

    Finish conversion from geographical to tmpc

--- test/test_TMPC.py
+++ test/test_TMPC.py
@@ -1,15 +1,15 @@
 from tmpc import *
-import sys
-
-threshold = 0.00001
 
 
-def within_threshold(expected, actual):
+def within_threshold(threshold, expected, actual):
     return expected - threshold < actual < expected + threshold
 
 
 def test_convert_to_geographic():
-    #
+    geographic_threshold = 0.00001
+    projection_threshold = 1
+
+    # Singapore's svy21 system's origin of projection and reference spheroid
     svy21_origin = Origin(1.3666666666, 103.83333333, 28001.6420, 38744.5720, 1.0000)
     svy21_spheroid = Spheroid(6378137.0000, 1 / 298.257223563)
 
@@ -21,10 +21,12 @@ def test_convert_to_geographic():
     lat1 = 1.302253804056663
     long1 = 103.85820845283774
 
-    actual_lat1, actual_long1 = converter.convert_to_geographic(north1, east1)
+    result_lat1, result_long1 = converter.convert_to_geographic(north1, east1)
 
-    assert within_threshold(lat1, actual_lat1)
-    assert within_threshold(long1, actual_long1)
+    assert within_threshold(geographic_threshold, lat1, result_lat1)
+    assert within_threshold(geographic_threshold, long1, result_long1)
 
+    result_north1, result_east1 = converter.convert_to_projection(lat1, long1)
 
-
+    assert within_threshold(projection_threshold, east1, result_east1)
+    assert within_threshold(projection_threshold, north1, result_north1)
--- tmpc/tmpc.py
+++ tmpc/tmpc.py
@@ -19,6 +19,30 @@ def sec(x):
     return 1.0 / cos(x)
 
 
+def cos2(x):
+    return cos(x) * cos(x)
+
+
+def cos3(x):
+    return cos(x) * cos(x) * cos(x)
+
+
+def cos4(x):
+    return cos2(x) * cos2(x)
+
+
+def cos5(x):
+    return cos3(x) * cos(x) * cos(x)
+
+
+def cos6(x):
+    return cos3(x) * cos3(x)
+
+
+def cos7(x):
+    return cos5(x) * cos(x) * cos(x)
+
+
 # This class represents the origin of projection and is required by the Tmpc class to do any conversions.
 class Origin:
     def __init__(self, latitude, longitude, false_easting, false_northing, central_meridian_scale_factor):
@@ -110,8 +134,51 @@ class Tmpc:
 
         return round(lat, 10), round(long, 10)
 
-    def convert_to_projection(self):
-        raise NotImplementedError
+    def convert_to_projection(self, lat, long):
+        params = Parameters(self.origin, self.spheroid)
+
+        m = params.a * (params.A0 * rad(lat) -
+                        params.A2 * sin(2 * rad(lat)) +
+                        params.A4 * sin(4 * rad(lat)) -
+                        params.A6 * sin(6 * rad(lat)))
+        rho = params.a * (1 - params.e2) / pow(1 - params.e2 * sin2(rad(lat)), 3.0 / 2.0)
+        v = params.a / sqrt(1 - params.e2 * sin2(rad(lat)))
+        psi = v / rho
+        psi2 = psi * psi
+        psi3 = psi * psi * psi
+        psi4 = psi2 * psi2
+        t = tan(rad(lat))
+        t2 = t * t
+        t4 = t2 * t2
+        t6 = t4 * t2
+        omega = rad(long - params.long_origin)
+        omega2 = omega * omega
+        omega4 = omega2 * omega2
+        omega6 = omega2 * omega4
+        omega8 = omega4 * omega4
+
+        north_term1 = omega2 / 2.0 * v * sin(rad(lat)) * cos(rad(lat))
+        north_term2 = omega4 / 24.0 * v * sin(rad(lat)) * cos3(rad(lat)) * (4 * psi2 + psi - t2)
+        north_term3 = omega6 / 720 * v * sin(rad(lat)) * cos5(rad(lat)) * \
+                      (8 * psi4 * (11 - 24 * t2) -
+                       28 * psi3 * (1 - 6 * t2) +
+                       psi2 * (1 - 32 * t2) -
+                       psi * 2 * t2 +
+                       t4)
+        north_term4 = omega8 / 40320 * v * sin(rad(lat)) * cos7(rad(lat)) * \
+                      (1385 - 3111 * t2 + 543 * t4 - t6)
+        north = params.north_false + params.k * (m - params.m0 + north_term1 + north_term2 + north_term3 + north_term4)
+
+        east_term1 = omega2 / 6 * cos2(rad(lat)) * (psi - t2)
+        east_term2 = omega4 / 120 * cos4(rad(lat)) * \
+                     (4 * psi3 * (1 - 6 * t2) +
+                      psi2 * (1 + 8 * t2) -
+                      psi * 2 * t2 +
+                      t4)
+        east_term3 = omega6 / 5040 * cos6(rad(lat)) * (61 - 479 * t2 + 179 * t4 - t6)
+        east = params.east_false + params.k * v * omega * cos(rad(lat)) * (1 + east_term1 + east_term2 + east_term3)
+
+        return north, east
 
 
 # This class represents the entire set of common parameters needed conversion between the two coordinate systems. They
@@ -132,12 +199,12 @@ class Parameters:
         self.e4 = self.e2 * self.e2
         self.e6 = self.e2 * self.e2 * self.e2
 
-        A0 = 1.0 - self.e2 / 4.0 - 3.0 * self.e4 / 64.0 - 5.0 * self.e6 / 256.0
-        A2 = 3.0 / 8.0 * (self.e2 + self.e4 / 4.0 + 15.0 * self.e6 / 128.0)
-        A4 = 15.0 / 256.0 * (self.e4 + 3.0 * self.e6 / 4.0)
-        A6 = 35.0 * self.e6 / 3072.0
+        self.A0 = 1.0 - self.e2 / 4.0 - 3.0 * self.e4 / 64.0 - 5.0 * self.e6 / 256.0
+        self.A2 = 3.0 / 8.0 * (self.e2 + self.e4 / 4.0 + 15.0 * self.e6 / 128.0)
+        self.A4 = 15.0 / 256.0 * (self.e4 + 3.0 * self.e6 / 4.0)
+        self.A6 = 35.0 * self.e6 / 3072.0
 
-        self.m0 = self.a * (A0 * rad(self.lat_origin) -
-                            A2 * sin(2 * rad(self.lat_origin)) +
-                            A4 * sin(4 * rad(self.lat_origin)) -
-                            A6 * sin(6 * rad(self.lat_origin)))
+        self.m0 = self.a * (self.A0 * rad(self.lat_origin) -
+                            self.A2 * sin(2 * rad(self.lat_origin)) +
+                            self.A4 * sin(4 * rad(self.lat_origin)) -
+                            self.A6 * sin(6 * rad(self.lat_origin)))
